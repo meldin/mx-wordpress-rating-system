5498a64b6288843ffcd13c301578f8bd10d6fc6e
meldin <meldinxavier@gmail.com>
2015-06-23 11:06:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
 
+// Register Style
+function mx_rating_styles() {
+
+	wp_deregister_style( 'Rating Style' );
+	wp_register_style( 'Rating Style', 'css/style.css', false, '1.0.0', 'all' );
+	wp_enqueue_style( 'Rating Style' );
+
+}
+
+// Hook into the 'wp_enqueue_scripts' action
+add_action( 'wp_enqueue_scripts', 'mx_rating_styles' );
