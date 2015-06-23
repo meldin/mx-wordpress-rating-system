fbfe8691a6d0ef28430eaf705216128d4d9c5376
meldin <meldinxavier@gmail.com>
2015-06-23 11:11:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-
+/*
+  Plugin Name: MX Wordpress Rating System
+  Plugin URI: https://www.facebook.com/meldin.xavier
+  Description: Basic Rating Plugin for posts.
+  Version: 1.0
+  Author: Meldin Xavier
+  Author URI: https://www.facebook.com/meldin.xavier
+  License: GPLv2+
+  Text Domain: mx-wordpress-rating-system
+*/
 // Register Style
 function mx_rating_styles() {
 
